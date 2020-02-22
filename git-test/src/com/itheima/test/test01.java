9a45201e1568c8cee22eef767255c0afd417faae
ceshi <ceshi@itcast.cn>
2020-02-22 22:13:54 +0800

    在test工程中的test01.java文件中新添加了一个test02方法

--- git-test/src/com/itheima/test/test01.java
+++ git-test/src/com/itheima/test/test01.java
@@ -5,4 +5,10 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("hello! git!");
     }
+
+    public void test02(){
+        int i = 0;
+        i += 6;
+        System.out.println("i="+i);
+    }
 }
